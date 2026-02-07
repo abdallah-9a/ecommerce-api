6b3528af9d906caba53cf5655ef189d2bb4e7acf
abdallah-rabie <abdallah.rabie.fayez@gmail.com>
2026-02-07 23:52:32 +0200

    feat: implement cache-aside pattern for products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.core.cache import cache
 from rest_framework import generics, filters
+from rest_framework.response import Response
 from .models import Product, Category
 from .serializers import (
     ProductListSerializer,
@@ -25,12 +26,46 @@ class ProductListCreateView(generics.ListCreateAPIView):
             return ProductListSerializer
 
         return ProductSerializer
+    
+    def list(self, request, *args, **kwargs):
+        """
+        Override the default list method to implement Cache-Aside pattern.
+        """
+        if request.query_params.get("search"):
+            return super().list(request, *args, **kwargs)
+
+        cache_key = f"product_list_{request.get_full_path()}"
+        cached_data = cache.get(cache_key)
+
+        if cached_data:
+            return Response(cached_data)
+
+        response = super().list(request, *args, **kwargs)
+        cache.set(cache_key, response.data, timeout=60 * 15)
+
+        return response
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+        # Invalidation 
+        cache.delete_pattern("product_list_*")
 
 
 class ProductView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.select_related("category").all()
     serializer_class = ProductSerializer
     permission_classes = [IsAdminOrReadOnly]
+
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        cache.delete_pattern("product_list_*")
+    
+
+    def perform_destroy(self, instance):
+        instance.delete()
+        cache.delete_pattern("product_list_*")
 
 
 class CategoryListCreateView(generics.ListCreateAPIView):
